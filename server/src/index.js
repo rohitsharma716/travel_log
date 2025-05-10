b878d08bc6e70a4038efabfff8c89976f3f4c4b6
ROHIT SHARMA <102727841+rohitsharma716@users.noreply.github.com>
2025-05-10 13:19:58 +0530

    change title and logo

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,25 @@
+require('dotenv').config();
 const express = require('express')
 const morgan  = require('morgan');
 const helmet = require('helmet');
 const cors    =  require('cors');
 const middleware =  require('./middlewares');
-const { mongoose } = require('mongoose');
-require('dotenv').config();
+const  mongoose  = require('mongoose');
 const logs = require('./routes/logs');
 const auth = require('./routes/auth');
 
 const app = express();
 
-mongoose.connect(process.env.DATABASE_URL ,{
+mongoose.connect('mongodb://localhost/travel-log' ,{
     useNewUrlParser : true,
-})
+    useUnifiedTopology: true,
+}).then(()=> console.log("mongoDB connected"))
+  .catch( err => console.error('MongoDb connection error: ' ,err))
 
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN ||  '*',
     optionsSuccessStatus: 200, // For legacy browser support
 
 }));
